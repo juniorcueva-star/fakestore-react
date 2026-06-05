0826bc3a32bee60586fa2f40f42a531bc3f97c26
junior777 <junior777fabian@gmail.com>
2026-06-05 18:53:35 -0500

    configura estructura inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app">
+      <h1>fakestore-react</h1>
+      <p>SPA con Vite, React 19 y TypeScript.</p>
+    </main>
+  );
+}
+
+export default App;
